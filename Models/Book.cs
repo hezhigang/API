5617083f78e6e426f077713ac971366b74c2aac2
Zhigang He <hezhigang1024@gmail.com>
2023-01-12 23:03:47 -0500

    add cover pic of book

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,6 +8,7 @@ namespace API.Models
 		public int Id { get; set; }
 		public string Title { get; set; } = string.Empty;
 		public string Author { get; set; } = string.Empty;
+		public string CoverImageUrl { get; set;} = string.Empty;
 		public string Description { get; set; } = string.Empty;
 		public decimal Price { get; set; }
 		public string Sku { get; set; } = string.Empty;
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # API
-API for eCommence website for C# project
+API for e-commerce web application for C# project
+
+# API
+
+## all books
+
+http://localhost:5041/api/Books
+
+# links
+
+- <http://localhost:5041/swagger/index.html>
+- 
